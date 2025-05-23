2c7b334c43de4e65ee66c6d4ea7e2a6ca79a7daf
Karol Arabasz <karolarabasz@student.agh.edu.pl>
2025-05-23 21:22:22 +0200

    Script which shows password details of users

---
+++ PasswordDetails.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Name of user who you want to analyze:"
+read name
+
+username=$name
+
+if grep -q "^$username" /etc/shadow; then
+	echo "Username: $username"
+	
+	HashedPassword=$(grep "^$username" /etc/shadow | cut -d':' -f2)
+	echo "Hashed password: $HashedPassword"
+
+	DaysToChangePassword=$(grep "^$username" /etc/shadow | cut -d':' -f4)
+	echo "Number of days after which the password can be changed again: $DaysToChangePassword"
+
+	DaysOfValidPassword=$(grep "^$username" /etc/shadow | cut -d':' -f5)
+	echo "Number of days the password is valid: $DaysOfValidPassword"
+	
+	DaysToNotifications=$(grep "^$username" /etc/shadow | cut -d':' -f6)
+	echo "Number of days before password expiration when user receives notification: $DaysToNotifications"
+else 
+	echo "No user with username $username"
+fi
